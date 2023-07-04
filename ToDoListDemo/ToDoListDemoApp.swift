3f3177c6c37e83cf0a6b150f6b1e85fe8b158b84
Hannah Kim <kyki73394@gmail.com>
2023-07-04 12:29:31 +0900

    Initial Commit

---
+++ ToDoListDemo/ToDoListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListDemoApp.swift
+//  ToDoListDemo
+//
+//  Created by scholar on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
