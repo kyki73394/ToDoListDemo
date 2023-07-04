63e8f4854c9d8023e7407aac31fef0e1457a2da8
Hannah Kim <kyki73394@gmail.com>
2023-07-04 14:15:04 +0900

    re: Iteration 0

--- ToDoListDemo/ContentView.swift
+++ ToDoListDemo/ContentView.swift
@@ -4,54 +4,32 @@
 //
 //  Created by scholar on 7/4/23.
 //
-
 import SwiftUI
-
 struct ContentView: View {
-    
-    @State var toDoItems: [ToDoItem] = []
-    @State private var showNewTask = false
-    
     var body: some View {
         VStack {
             HStack {
                 Text("To Do List")
                      .font(.system(size: 40))
                      .fontWeight(.black)
-                
+
                 Spacer()
-                
+
                 Button(action: {
-                    self.showNewTask = true
+
                 }) {
                 Text("+")
                 }
-                
+
                 Spacer()
-                     
+
             }
             .padding()
-            
+
         }
         .padding()
-        
-        List {
-            ForEach (toDoItems) { toDoItem in
-                if toDoItem.isImportant == true {
-                    Text("‼️" + toDoItem.title)
-                } else {
-                    Text(toDoItem.title)
-                }
-            }
-        }
-        .listStyle(.plain)
-
-        if showNewTask {
-            NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
-        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
